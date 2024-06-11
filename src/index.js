256c653351eeb9bc6cddccb265ac1a02acec8c06
Ratha0122 <thoeurnratha1@gmail.com>
2024-06-11 17:16:12 +0700

    1222

--- src/index.js
+++ src/index.js
@@ -1,24 +1,47 @@
-document.addEventListener('alpine:init', () => {
-    Alpine.data('carousel', () => ({
-        currentIndex: 0,
-        images: [
-            '../images/b6b2ae78-749a-48e0-bc3a-0465d8ba4bc7.webp',
-            '../images/a7aa6cc8-916c-43b5-baaf-619fdb18a2d1.jpg',
-            '../images/31adef74-8d57-4bea-a03f-0bf303507f04.webp'
-        ],
-        prev() {
-            if (this.currentIndex > 0) {
-                this.currentIndex--;
-            } else {
-                this.currentIndex = this.images.length - 1;
-            }
+function OpenNav(){
+    document.getElementById("Nav").style.width= "100%"
+}
+function CloseNav(){
+    document.getElementById("Nav").style.width= "0"
+}
+
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    centereSlide: true,
+    slidesPerView: 1,
+    //effect : "cube",
+    grabCursor: true,
+    loop: true,
+    autoplay: {
+        delay: 5000,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+        320: {
+            slidesPerView: 1,
         },
-        next() {
-            if (this.currentIndex < this.images.length - 1) {
-                this.currentIndex++;
-            } else {
-                this.currentIndex = 0;
+        480: {
+            slidesPerView: 1,
+        },
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    cubeEffect:{
+        shadow: true,
+        slideShadows: true,
+        shadowOffset:20,
+        shadowScale: 0.94,
+    },
+    on: {
+        autoplayTimeLeft(s, time, progress){
+            const currentSlide = s.slides[s.activeIndex];
+            const progressBar = currentSlide.querySelector(".progressBar")
+            if(progressBar){
+                const widthPercentage = parseFloat(progress) * 100;
+                progressBar.style.width =widthPercentage + "%"
             }
         }
-    }));
-});
+    }
+})
